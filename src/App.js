2f0a04f1ac2635d2ebe23d4f88d8ca82a7da3dd9
Azlina Yeo <azlinayeo@gmail.com>
2020-08-15 15:25:58 +0100

    added input text and button

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useNotification } from './context/NotificationProvider';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
+  const dispatch = useNotification();
+  const [inputText, setInputText] = useState("");
+  const [notificationType, setNotificationType] = useState("SUCCESS");
 
+  const handleAddNotification = () => {
+    dispatch({
+      msg: inputText,
+      type: notificationType,
+    })
+    setInputText("");
+  }
+
+  return (
+    <div className="app">
+      <input className="notification__text" placeholder="Notification Type" type="text" value={inputText} onChange={e => setInputText(e.target.value)} />
+      <select className="notification__type" onChange={e => setNotificationType(e.target.value)}>
+        <option value="SUCCESS">success</option>
+        <option value="ERROR">error</option>
+      </select>
+      <button onClick={handleAddNotification}>Add Notification</button>
     </div>
   );
 }
