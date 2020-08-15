54d9a29e738bd2ad30e7c0291cef635d3892a074
Azlina Yeo <azlinayeo@gmail.com>
2020-08-15 15:28:27 +0100

    added reducer and context

--- src/context/NotificationProvider.js
+++ src/context/NotificationProvider.js
@@ -1,7 +1,8 @@
-import React, { useReducer } from 'react';
+import React, { useReducer, useContext, createContext } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 import Notification from '../components/Notification';
 
+const NotificationContext = createContext()
 const NotificationProvider = (props) => {
     const [state, dispatch] = useReducer((state, action) => {
         switch (action.type) {
@@ -10,30 +11,30 @@ const NotificationProvider = (props) => {
             case "REMOVE_NOTIFICATION":
                 return state.filter(elem => elem.id !== action.id);
             default: return state;
-
         }
-    }, [
-        {
-            id: uuidv4(),
-            type: "SUCCESS",
-            msg: "Hello World"
-        },
-        {
-            id: uuidv4(),
-            type: "SUCCESS",
-            msg: "Hello World 2"
-        }
-    ]);
+    }, []);
 
     return (
-        <div>
-            <div className={"notification-wrapper"}>
+        <NotificationContext.Provider value={dispatch}>
+            <div className={"notification__wrapper"}>
                 {state.map(notification =>
                     <Notification dispatch={dispatch} key={notification.id} {...notification} />)}
             </div>
             {props.children}
-        </div>
+        </NotificationContext.Provider>
     )
 };
 
+export const useNotification = () => {
+    const dispatch = useContext(NotificationContext);
+    return (props) => {
+        dispatch({
+            type: "ADD_NOTIFICATION",
+            payload: {
+                id: uuidv4(),
+                ...props
+            }
+        })
+    }
+}
 export default NotificationProvider;
