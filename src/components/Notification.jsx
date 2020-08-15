fea1a44c84519aa5584519832936373b40e016d3
Azlina Yeo <azlinayeo@gmail.com>
2020-08-15 13:40:16 +0100

    coded notification

--- src/components/Notification.jsx
+++ src/components/Notification.jsx
@@ -1,30 +1,55 @@
 import React, { useState, useEffect } from 'react';
 
 const Notification = (props) => {
+    const [exit, setExit] = useState(false);
     const [width, setWidth] = useState(0);
     const [intervalId, setIntervalId] = useState(null);
 
     const handleStartTimer = () => {
         const id = setInterval(() => {
-            setWidth((prev) => prev < 100 ? prev + 0.5 : prev);
-
+            setWidth((prev) => {
+                if (prev < 100) return 0.5 + prev;
+                clearInterval(id);
+                return prev;
+            });
         }, 20);
+
         setIntervalId(id);
     }
+
+
     const handlePauseTimer = () => {
         clearInterval(intervalId);
     }
+
+    const handleCloseNotification = () => {
+        handlePauseTimer();
+        setExit(true);
+        setTimeout(() => {
+            //remove from state 
+            props.dispatch({
+                type: "REMOVE_NOTIFICATION",
+                id: props.id,
+            })
+        }, 400);
+    }
+
     useEffect(() => {
         handleStartTimer();
-    }, [])
+    }, []);
+
+    useEffect(() => {
+        if (width === 100) handleCloseNotification();
+    }, [width]);
+
     return (
         <div
             onMouseEnter={handlePauseTimer}
             onMouseLeave={handleStartTimer}
-            className={`notification-item ${props.type === "SUCCESS" ? "success" : "error"}`}>
+            className={`notification-item ${props.type === "SUCCESS" ? "success" : "error"} ${exit ? "exit" : ""}`}>
 
             <p>{props.msg}</p>
-            <div className={"bar"} style={{ width: `${width}%` }} />
+            <div className={"bar"} style={{ width: `${width}% ` }} />
         </div >
     );
 };
--- src/context/NotificationProvider.js
+++ src/context/NotificationProvider.js
@@ -1,20 +1,35 @@
-import React from 'react';
+import React, { useReducer } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 import Notification from '../components/Notification';
 
 const NotificationProvider = (props) => {
-    const notifications = [
+    const [state, dispatch] = useReducer((state, action) => {
+        switch (action.type) {
+            case "ADD_NOTIFICATION":
+                return [...state, { ...action.payload }];
+            case "REMOVE_NOTIFICATION":
+                return state.filter(elem => elem.id !== action.id);
+            default: return state;
+
+        }
+    }, [
         {
-            id: uuidv4,
+            id: uuidv4(),
             type: "SUCCESS",
             msg: "Hello World"
+        },
+        {
+            id: uuidv4(),
+            type: "SUCCESS",
+            msg: "Hello World 2"
         }
-    ];
+    ]);
+
     return (
         <div>
             <div className={"notification-wrapper"}>
-                {notifications.map(notification =>
-                    <Notification key={notification.id} {...notification} />)}
+                {state.map(notification =>
+                    <Notification dispatch={dispatch} key={notification.id} {...notification} />)}
             </div>
             {props.children}
         </div>
